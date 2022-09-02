34d6ad47364f7f8869a1d606ace1bed5b0c493c4
Justine Stroup <jjstroup@mail.fhsu.edu>
2022-09-02 15:24:47 -0600

    Fixed error where user could only input one string

---
+++ IsomorphicStrings.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class IsomorphicStrings
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        char answer = 'y';
+        do
+        {
+        // Get user input
+        System.out.println("Enter the first string followed by the enter key:");
+        String stringOne = input.nextLine();
+
+        System.out.println("Enter the second string followed by the enter key:");
+        String stringTwo = input.nextLine();
+
+        // Isomorphic strings must be same length, so if different lengths, not isomorphic
+        if (stringOne.length() != stringTwo.length())
+            System.out.println("The strings are not isomorphic.");
+        
+        // Strings are same length, check if isomorphic
+        else
+            System.out.println(isIsomorphic(stringOne, stringTwo));
+        
+        System.out.println("\nWould you like to compare strings again? \n Type 'Y' for yes or 'N' for no: ");
+        answer = input.next().charAt(0);
+        } while ((answer == 'Y') || (answer == 'y'));
+    }
+    private static String transformString(String s)
+    {
+        Map<Character, Integer> indexMapping = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c1 = s.charAt(i);
+            
+            if(!indexMapping.containsKey(c1))
+                indexMapping.put(c1, i);
+            builder.append(Integer.toString(indexMapping.get(c1)));
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
+    public static boolean isIsomorphic(String s, String t)
+    {
+        // Compare the two newly transformed strings
+        return transformString(s).equals(transformString(t));
+    }
+}
